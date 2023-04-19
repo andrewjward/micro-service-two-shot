e8dff87a30a2c334009afc695c86b02ef63edcab
Andrew <a.drew517@gmail.com>
2023-04-19 13:59:50 -0400

    added to settings

--- hats/api/hats_project/settings.py
+++ hats/api/hats_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "hats_rest.apps.HatsRestConfig",
     "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
