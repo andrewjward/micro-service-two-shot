d703800400031c37aab4ba13676a2884dee20bd8
ChristopherRoldan <cproldan53@gmail.com>
2023-04-19 18:20:02 -0400

    views.py

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BinVO(models.Model):
+    closet_name = models.CharField(max_length=100)
+    bin_number = models.PositiveSmallIntegerField()
+    bin_size = models.PositiveSmallIntegerField()
+    import_href = models.URLField(max_length=200, null=True, unique=True)
+
+
+class Shoe(models.Model):
+    manufacturer = models.CharField(max_length=50)
+    model_name = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    picture_url = models.URLField(max_length=200, null=True, blank=True)
+    bin = models.ForeignKey(
+        "BinVO", related_name="shoes", on_delete=models.CASCADE, null=True
+    )
+
+
+# Create your models here.
--- shoes/api/shoes_rest/views.py
+++ shoes/api/shoes_rest/views.py
@@ -1,3 +1,76 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+from common.json import ModelEncoder
+from .models import BinVO, Shoe
 # Create your views here.
+class BinVoEncoder(ModelEncoder):
+    model= BinVO
+    properties= ["import_href", "closet_name", "bin_number"]
+
+class ShoeDetailEncoder(ModelEncoder):
+    model= Shoe
+    properties= ["manufacturer", "model_name", "color", "picture_url", "bin",]
+    encoders={"bin": BinVoEncoder()}
+
+class ShoeListEncoder(ModelEncoder):
+    model=Shoe
+    properties= [
+        "manufacturer",
+        "model_name",
+        "color",
+        "picture_url",
+        "id",
+        "bin",
+        ]
+    encoders={"bin":BinVoEncoder()}
+
+@require_http_methods(["GET", "POST"])
+def api_list_shoes(request, bin_vo_id=None):
+    # GET Request Handling
+    if request.method == "GET":
+        if bin_vo_id is not None:
+            shoe = Shoe.objects.filter(bin=bin_vo_id)
+        else:
+            shoe = Shoe.objects.all()
+        return JsonResponse(
+            {"shoes": shoe},
+            encoder=ShoeListEncoder,
+        )
+    else:
+        content= json.loads(request.body)
+        import_href = f"/api/bins/{content['bin']}/"
+        bin = BinVO.objects.get(import_href=import_href)
+        content["bin"]=bin
+        shoes= Shoe.objects.create(**content)
+        return  JsonResponse(
+            shoes,
+            encoder=ShoeDetailEncoder,
+            safe=False
+        )
+
+@require_http_methods(["GET", "DELETE"])
+def api_show_shoe(request, id):
+    if request.method == "GET":
+        try:
+            shoe= Shoe.objects.get(id=id)
+            return JsonResponse(
+                shoe,
+                encoder=ShoeDetailEncoder,
+                safe=False,
+            )
+        except Shoe.DoesNotExist:
+            return JsonResponse({"message": "invalid shoe id"}, status=400,)
+    elif request.method == "DELETE":
+        count,_= Shoe.objects.filter(id=id).delete()
+        return JsonResponse({"Deleted": count >0})
+    else:
+        content= json.loads(request.body)
+        Shoe.objects.filter(id=id).update(**content)
+        shoe= Shoe.objects.get(id=id)
+        return JsonResponse(
+            shoe,
+            encoder=ShoeDetailEncoder,
+            safe=False,
+        )
